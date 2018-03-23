0228ca09117373f695bd3cd2a97628368bc6766d
fermi_dirac <ManueleBryan@gmail.com>
2018-03-23 11:53:53 -0700

    added bin

--- server/index.js
+++ server/index.js
@@ -1,1 +1,9 @@
-/* @TODO */
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.port || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
